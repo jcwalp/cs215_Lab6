ecbe02ad5f9a035e5d4f79a52095ffd2ff7010fa
Jon <jwalp@sjci.com>
2019-11-13 12:59:31 -0500

    idk

---
+++ cs215_Lab6_2019/a_and_b/a.js
@@ -0,0 +1,24 @@
+
+
+var http = require('http');
+
+function fetchPage() {
+  console.log('fetching page');
+  http.get({ host: 'herokuapp.com', path: '/?delay=2000' }, function(res) {
+    console.log('data returned from requesting page');
+  }).on('error', function(e) {
+    console.log("There was an error" + e);
+  });
+}
+
+function fetchApi() {
+  console.log('fetching api');
+  http.get({ host: 'herokuapp.com', path: '/?delay=2000' }, function(res) {
+    console.log('data returned from the api');
+  }).on('error', function(e) {
+    console.log("There was an error" + e);
+  });
+}
+
+fetchPage();
+fetchApi();
